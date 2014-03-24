48ca5762515a81bd7b7f434c2793c5ceff01c895
Nicole Arnold <nicole@alleyinteractive.com>
2014-03-24 00:09:14 -0400

    add taxonomy & filter, store selection, change meta box to drop down (todo: preselect dropdown based on session value)

--- handbook.php
+++ handbook.php
@@ -33,11 +33,23 @@ class WPorg_Handbook {
 	function __construct() {
 		add_filter( 'user_has_cap', array( $this, 'grant_handbook_caps' ) );
 		add_filter( 'init', array( $this, 'register_post_type' ) );
+		add_filter( 'init', array( $this, 'register_taxonomy' ) );
+		add_action( 'init', array( $this, 'session_initialize' ) );
 		add_action( 'admin_page_access_denied', array( $this, 'admin_page_access_denied' ) );
+		add_action( 'admin_menu', array( $this, 'taxonomy_meta_box' ) );
 		add_filter( 'post_type_link', array( $this, 'post_type_link' ), 10, 2 );
 		add_filter( 'pre_get_posts', array( $this, 'pre_get_posts' ) );
 		add_action( 'widgets_init', array( $this, 'handbook_sidebar' ), 11 ); // After P2
 		add_action( 'wporg_email_changes_for_post_types', array( $this, 'wporg_email_changes_for_post_types' ) );
+		add_filter( 'parse_query' , array( $this, 'filter_pages' ) );
+		add_action( 'restrict_manage_posts', array( $this, 'restrict_handbook_pages_by_type' ) );
+	}
+
+	function session_initialize() {
+
+	    if ( ! session_id() ) {
+	        session_start();
+	    }
 	}
 
 	function grant_handbook_caps( $caps ) {
@@ -59,7 +71,7 @@ class WPorg_Handbook {
 			'labels' => array(
 				'name' => 'Handbook Pages',
 				'singular_name' => 'Handbook Page',
-				'menu_name' => 'Handbook',
+				'menu_name' => 'Handbook Pages',
 			),
 			'public' => true,
 			'show_ui' => true,
@@ -74,12 +86,83 @@ class WPorg_Handbook {
 		) );
 	}
 
+	function register_taxonomy() {
+		register_taxonomy( 'handbook_type', 'handbook', array(
+			'label'                 => __( 'Handbook Type', 'wporg' ),
+			'public'                => true,
+			'rewrite'               => array( 'slug' => 'handbooks' ),
+			'sort'                  => false,
+		) );
+	}
+
+	function taxonomy_meta_box() {
+	    if ( ! is_admin() )
+	        return;
+		remove_meta_box( 'tagsdiv-handbook_type','handbook','side') ;
+    	add_meta_box( 'handbook_type_box_ID', __( 'Handbook Type' ), array( $this, 'add_taxonomy_dropdown' ), 'handbook', 'side', 'core' );
+	}
+
+	// This function gets called in edit-form-advanced.php
+	function add_taxonomy_dropdown( $post ) {
+
+	    $output = '<input type="hidden" name="taxonomy_noncename" id="taxonomy_noncename" value="' . wp_create_nonce( 'taxonomy_handbook_type' ) . '" />';
+
+	    // Get all handbook_type taxonomy terms
+	    $handbook_types = get_terms( 'handbook_type', 'hide_empty=0' );
+		$output .= "<select name='post_handbook_type' id='post_handbook_type'>";
+		$names = wp_get_object_terms( $post->ID, 'handbook_type' );
+		    foreach ( $handbook_types as $handbook_type ) {
+				$selected = ( !is_wp_error( $names ) && !empty( $names ) && !strcmp( $handbook_type->slug, $names[0]->slug ) ) ? ' selected' : '';
+		    	$output .= sprintf( "<option class='handbook_type-option' value='%s' %s>%s</option>\n", $handbook_type->slug, $selected, $handbook_type->name );
+	   		}
+		$output .= "</select>";
+		echo $output;
+	}
+
 	function admin_page_access_denied() {
 		if ( ! current_user_can( 'read' ) ) {
 			wp_redirect( admin_url( 'edit.php?post_type=handbook' ) );
 			exit;
 		}
 	}
+
+	function filter_pages( $query ) {
+	    global $pagenow;
+	    global $typenow;
+	    if ( $pagenow =='edit.php' && $typenow = 'handbook' ) {
+	    	$qv = &$query->query_vars;
+		    if ( isset( $qv['handbook_type'] ) && is_numeric( $qv['handbook_type'] ) ) {
+		        $term = get_term_by( 'id', $qv['handbook_type'], 'handbook_type' );
+		        $qv['handbook_type'] = $term->slug;
+        		$_SESSION['handbook_type'] = $term->term_id;
+		    } else {
+		    	if ( isset( $_SESSION['handbook_type'] ) ) {
+		        	$term = get_term_by( 'id', $_SESSION['handbook_type'], 'handbook_type' );
+					$qv['handbook_type'] = $term->slug;
+    			} else {
+			    	$terms = get_terms( 'handbook_type', 'hide_empty=0&fields=id=>slug' );
+			    	$qv['handbook_type'] = array_shift ( $terms );
+    			}
+		    }
+	    }
+	}
+
+	function restrict_handbook_pages_by_type() {
+	    global $typenow;
+	    global $wp_query;
+	    $selected = ( isset( $_SESSION['handbook_type'] ) ) ? $_SESSION['handbook_type'] : $wp_query->query['handbook_type'];
+	    if ( $typenow =='handbook' ) {
+	        wp_dropdown_categories( array(
+	            'taxonomy'        => 'handbook_type',
+	            'name'            => 'handbook_type',
+	            'orderby'         => 'name',
+	            'selected'        => $selected,
+	            'hierarchical'    => true,
+	            'depth'           => 3,
+	            'hide_empty'      => false, // show all listings
+	        ) );
+    	}
+    }
 
 	function post_type_link( $link, $post ) {
 		if ( $post->post_type === 'handbook' && $post->post_name === 'handbook' )
@@ -104,8 +187,9 @@ class WPorg_Handbook {
 	}
 
 	function wporg_email_changes_for_post_types( $post_types ) {
-		if ( ! in_array( 'handbook', $post_types ) )
+		if ( ! in_array( 'handbook', $post_types ) ) {
 			$post_types[] = 'handbook';
+		}
 		return $post_types;
 	}
 }
