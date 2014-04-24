7024aafb73235a03e7fc574ef8cb0c66299e3f8f
Nicole Arnold <nicole@alleyinteractive.com>
2014-04-24 15:04:34 -0400

    fix widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -5,7 +5,7 @@ class WPorg_Handbook_Widget extends WP_Widget {
 	protected $post_types = 'wp';
 
 	function __construct() {
-		apply_filters( 'handbook_post_types', $this->post_types );
+		$this->post_types = apply_filters( 'handbook_post_types', $this->post_types );
 		if ( ! is_array( $this->post_types ) ) {
 			$this->post_types = (array) $this->post_types;
 		}
@@ -47,6 +47,8 @@ class WPorg_Handbook_Widget_for_Pages extends WPorg_Handbook_Widget {
 
 class WPorg_Handbook_Pages_Widget extends WP_Widget_Pages {
 
+	protected $post_types = 'wp';
+
 	function __construct() {
 		$widget_ops = array('classname' => 'widget_wporg_handbook_pages', 'description' => __( 'Your site&#8217;s Handbook Pages') );
 		WP_Widget::__construct('handbook_pages', __('Handbook Pages'), $widget_ops);
@@ -60,9 +62,20 @@ class WPorg_Handbook_Pages_Widget extends WP_Widget_Pages {
 
 	function handbook_post_type( $args ) {
 		$post = get_post();
+
+		$this->post_types = apply_filters( 'handbook_post_types', $this->post_types );
+		if ( ! is_array( $this->post_types ) ) {
+			$this->post_types = (array) $this->post_types;
+		}
+		$this->post_types = array_map( array( $this, 'append_suffix' ), $this->post_types );
+
 		if ( in_array( $post->post_type, $this->post_types ) ) {
 			$args['post_type'] = $post->post_type;
 		}
 		return $args;
+	}
+
+	function append_suffix( $t ) {
+		return $t . '-handbook';
 	}
 }
